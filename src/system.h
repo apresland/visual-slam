73b5565dbe2f6ef522043a722391063d802c4cc7
Andrew Presland <andrew.presland@gmail.com>
2021-09-26 11:56:59 +0200

    refactor: Moved entry point "main" into system.h

--- src/system.h
+++ src/system.h
@@ -24,4 +24,13 @@ private:
     bool initialized_ = false;
 };
 
+int main(int argc, char **argv)
+{
+    std::string path_to_sequence = "/data/kitti/odometry/dataset/sequences/00/";
+    std::shared_ptr<System> odometry = std::make_shared<System>(path_to_sequence);
+    odometry->Init();
+    odometry->Run();
+    return 0;
+}
+
 #endif //VISUAL_SLAM_SYSTEM_H
