5f679e66ee819fd161f502f23cb0b9d6ad0f60f9
Andrew Presland <andrew.presland@gmail.com>
2021-09-28 12:26:22 +0200

    feature: ROS master and RViz included in docker composition
    Updates docker-compose.yaml to spawn containers for ROS master and RViz and adds network "ros"

---
+++ docker_up.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+docker-compose up -d
+
