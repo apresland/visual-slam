4c8be69015ba7e7f01afd65c11288a887d77703e
Andrew Presland <andrew.presland@gmail.com>
2021-09-30 11:03:11 +0200

    feature: Publish pose and paths for visualization on RViz

--- src/system.cpp
+++ src/system.cpp
@@ -5,6 +5,7 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 #include <sophus/se3.hpp>
+#include <geometry_msgs/PoseStamped.h>
 #include "system.h"
 #include "viewer.h"
 #include "frontend.h"
@@ -20,6 +21,9 @@ System::System(ros::NodeHandle& node_handle)
     , calibration_file_(path_root_ + "/calib.txt") {
     image_pub_00_ = image_trans_.advertiseCamera("grayscale/left/image", 1);
     image_pub_01_ = image_trans_.advertiseCamera("grayscale/right/image", 1);
+    pose_pub_ = node_handle_.advertise<geometry_msgs::PoseStamped>("pose", 1);
+    path_publisher_ = node_handle_.advertise<nav_msgs::Path>("path", 1);
+    path_publisher_truth_ = node_handle_.advertise<nav_msgs::Path>("path_truth", 1);
 }
 
 bool System::Init() {
@@ -70,6 +74,8 @@ void System::Run() {
     frontend->setViewer(viewer);
     frontend->setBackend(backend);
 
+    loadGroundTruthPoses();
+
     cv_bridge::CvImage cv_bridge;;
     std::shared_ptr<Sequence::StereoPair> prev_element = nullptr;
     for (std::shared_ptr<Sequence::StereoPair> element : sequence_->elements_)
@@ -100,6 +106,9 @@ void System::Run() {
         publishRightImage(image_msg_01);
 
         frontend->pushback(image_00, image_01);
+        Sophus::SE3d pose = frontend->getPose();
+        publishPose(pose);
+
         ros::spinOnce();
     }
 
@@ -124,4 +133,75 @@ void System::publishRightImage(const sensor_msgs::ImagePtr &image) {
             = image->header.frame_id;
 
     image_pub_01_.publish(image, camera_info_msg);
+}
+
+void System::publishPose(Sophus::SE3d pose) {
+
+    geometry_msgs::PoseStamped pose_msg;
+    pose_msg.header.frame_id
+        = "base_link";
+
+    pose_msg.pose.position.x = pose.translation().x();
+    pose_msg.pose.position.y = pose.translation().z();
+    pose_msg.pose.position.z = pose.translation().y();
+
+    pose_msg.pose.orientation.x = 0.0;
+    pose_msg.pose.orientation.y = 0.0;
+    pose_msg.pose.orientation.z = 0.0;
+    pose_msg.pose.orientation.w = 0.0;
+
+    pose_pub_.publish(pose_msg);
+
+    path_msg_.header.frame_id = pose_msg.header.frame_id;
+    path_msg_.poses.emplace_back(pose_msg);
+    path_publisher_.publish(path_msg_);
+    path_publisher_truth_.publish(path_msg_truth_);
+}
+
+void System::loadGroundTruthPoses() {
+
+    std::string filepath = "/data/kitti/odometry/dataset/poses/";
+    const std::string filename = filepath + "/00.txt";
+    std::vector<cv::Mat> poses;
+
+    std::ifstream file;
+    file.open(filename);
+    if (!file) {
+        throw std::runtime_error("Could not load timestamps file: " + filepath);
+    }
+    poses.clear();
+    std::string  line;
+    while (std::getline(file, line)) {
+        std::istringstream input(line);
+        cv::Mat pose = cv::Mat_<double>(3, 4);
+        input >> pose.at<double>(0);
+        input >> pose.at<double>(1);
+        input >> pose.at<double>(2);
+        input >> pose.at<double>(3);
+        input >> pose.at<double>(4);
+        input >> pose.at<double>(5);
+        input >> pose.at<double>(6);
+        input >> pose.at<double>(7);
+        input >> pose.at<double>(8);
+        input >> pose.at<double>(9);
+        input >> pose.at<double>(10);
+        input >> pose.at<double>(11);
+
+        geometry_msgs::PoseStamped pose_msg;
+        pose_msg.header.frame_id
+                = "base_link";
+
+        pose_msg.pose.position.x = pose.at<double>(3);
+        pose_msg.pose.position.y = pose.at<double>(11);
+        pose_msg.pose.position.z = pose.at<double>(7);
+
+        pose_msg.pose.orientation.x = 0.0;
+        pose_msg.pose.orientation.y = 0.0;
+        pose_msg.pose.orientation.z = 0.0;
+        pose_msg.pose.orientation.w = 0.0;
+
+        path_msg_truth_.header.frame_id = pose_msg.header.frame_id;
+        path_msg_truth_.poses.emplace_back(pose_msg);
+    }
+    file.close();
 }
--- src/system.h
+++ src/system.h
@@ -9,6 +9,7 @@
 #include <Eigen/Core>
 #include "ros/ros.h"
 #include <image_transport/image_transport.h>
+#include "nav_msgs/Path.h"
 #include "sequence.h"
 #include "sensor/camera.h"
 
@@ -20,8 +21,11 @@ public:
     void Run();
 private:
 
+    void loadGroundTruthPoses();
+
     void publishLeftImage(const sensor_msgs::ImagePtr &image);
     void publishRightImage(const sensor_msgs::ImagePtr &image);
+    void publishPose(Sophus::SE3d pose);
 
     std::string path_root_;
     std::string calibration_file_;
@@ -31,7 +35,14 @@ private:
     image_transport::ImageTransport image_trans_;
     image_transport::CameraPublisher image_pub_00_;
     image_transport::CameraPublisher image_pub_01_;
+    nav_msgs::Path path_msg_;
+    nav_msgs::Path path_msg_truth_;
+    ros::Publisher pose_pub_;
+    ros::Publisher path_publisher_;
+    ros::Publisher path_publisher_truth_;
     bool initialized_ = false;
+
+    std::vector<cv::Mat> ground_truth_poses_;
 };
 
 int main(int argc, char **argv)
